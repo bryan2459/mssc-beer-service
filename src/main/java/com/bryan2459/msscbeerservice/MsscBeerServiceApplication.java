8b7324e54fc693159890360cc6f8b0e1696be0a6
bryan2459 <mvs360@gmail.com>
2022-08-24 18:36:10 +1000

    adding web data model closes #1

--- src/main/java/com/bryan2459/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/com/bryan2459/msscbeerservice/MsscBeerServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MsscBeerServiceApplication {
 
-    //adding to git
+    //adding to git again
 
     public static void main(String[] args) {
         SpringApplication.run(MsscBeerServiceApplication.class, args);
